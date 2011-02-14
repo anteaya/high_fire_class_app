cccf29891a0068405e19f02472657713ea365a1e
Anita Kuno <ay.kuno@gmail.com>
2011-02-14 14:46:31 -0500

    Created spec and tests for assignment model. All tests pass.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110214162254) do
+ActiveRecord::Schema.define(:version => 20110214192911) do
+
+  create_table "assignments", :force => true do |t|
+    t.string   "description"
+    t.date     "assigned_on"
+    t.date     "due_on"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", :force => true do |t|
     t.string   "first_name"
---
+++ spec/models/assignment_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Assignment do
+  it 'should have a description of type text' do
+    a = Assignment.new(:description => "some text")
+    a.save!
+    a.description.instance_of?(String)
+  end
+
+  it 'should have an assigned_on date' do
+    a = Assignment.new(:assigned_on => "Feb. 1, 2011")
+    a.save!
+    a.assigned_on.instance_of?(Date)
+  end
+
+  it 'should have a due_on date' do
+    a = Assignment.new(:due_on => "Feb. 15, 2011")
+    a.save!
+    a.due_on.instance_of?(Date)
+  end
+end
